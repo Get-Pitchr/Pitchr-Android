41bdfaf37eedab9b591fcae286a0d06d02fd9c01
Arin <arinray92@gmail.com>
2016-04-19 23:48:30 -0400

    Pitchr Ionic Files

---
+++ Pitchr/www/js/routes.js
@@ -0,0 +1,54 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+.state('tabsController.cartTabDefaultPage', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/cartTabDefaultPage.html',
+        controller: 'BarController'
+      }
+    }
+  })
+
+  .state('tabsController.cloudTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/cloudTabDefaultPage.html',
+        controller: 'cloudTabDefaultPageCtrl'
+      }
+    }
+  })
+  .state('showTab', {
+    url: '/showTab/:tabID',
+    templateUrl: 'templates/showTab.html',
+    controller: 'TabController'
+  })
+  
+  .state('closeTab', {
+    url: '/closeTab/:tabID',
+    templateUrl: 'templates/closeTab.html',
+    controller: 'TabController'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/page1/page3')
+
+  
+
+});
